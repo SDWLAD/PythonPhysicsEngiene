2e3fdb78dbbf444e5848d6c7dcc0437ff5f040e1
SDWLAD <sidorchukvladislav0@gmail.com>
2024-10-15 18:53:50 +0300

    Added mouse point gravitation

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pygame as pg
 from circleObject import CircleObject
 from physicsObject import PhysicsObject
 from settings import *
+import settings
 import math
 
 sc = pg.display.set_mode(SCREEN_SIZE)
@@ -10,9 +11,8 @@ clock = pg.time.Clock()
 
 objects = []
 
-# n, r = 50, 500
-# objects = [CircleObject(pg.Vector2(((r/n)*i) * math.cos(2 * math.pi * i / n) + SCREEN_SIZE[0]/2, ((r/n)*i) * math.sin(2 * math.pi * i / n) + SCREEN_SIZE[1]/2), 10) for i in range(n)]
-
+n, r = 50, 500
+objects = [CircleObject(pg.Vector2(((r/n)*i) * math.cos(2 * math.pi * i / n) + SCREEN_SIZE[0]/2, ((r/n)*i) * math.sin(2 * math.pi * i / n) + SCREEN_SIZE[1]/2), 10) for i in range(n)]
 
 while 1:
     for e in pg.event.get():
@@ -25,11 +25,14 @@ while 1:
         r = 20
         rotation = pg.Vector2(randint(-100, 100)/100, randint(-100, 100)/100).normalize()*r
         objects.append(CircleObject(pg.Vector2(pos[0], pos[1])+rotation, 10))
+    if pg.mouse.get_pressed()[2]:
+        x, y = pg.mouse.get_pos()
+        settings.gravity_point.x = x
+        settings.gravity_point.y = y
 
     for obj in objects:
         obj.update()
         obj.draw(sc)
-
-        
+             
     pg.display.flip()
     clock.tick(40)
