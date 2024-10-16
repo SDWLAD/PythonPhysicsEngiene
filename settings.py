86b4d4f8286dedfeded8b4ca7e04125258653046
SDWLAD <sidorchukvladislav0@gmail.com>
2024-10-16 22:17:39 +0300

    Returned old gravitation

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ import pygame as pg
 pg.init()
 
 SCREEN_SIZE = (1280, 720)
-gravity_direction = None
-gravity_point = pg.Vector2(1280/2, 720/2)
+gravity_direction = pg.Vector2(0, 1)
+gravity_point = None
